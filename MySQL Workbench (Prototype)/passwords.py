664c0fd128a36733fb5ddc4656635e2f48fa5548
Arub bin Ali <arubbinali@outlook.com>
2024-12-16 11:56:33 +0300

    `

---
+++ MySQL Workbench (Prototype)/passwords.py
@@ -0,0 +1,7 @@
+#Type in your MySQL account password inside the quotation marks to run `main.py`
+#Clear your password by deleting it and only leaving empty quotaion marks `""`assigned to `mysqlpassword` before syncing or cloning the repository
+
+mysqlpassword = ""
+
+def password():
+    return mysqlpassword
